1000f241ad516bfebed41e8938b9bd49c6c960f9
Aidan Engler <aidanbope@gmail.com>
2025-08-01 19:14:27 -0400

    Descriptive message about your updates

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html')
+        about: resolve(__dirname, 'about.html'),
+        automation: resolve(__dirname, 'automation.html'),
+        transportation: resolve(__dirname, 'transportaion.html'),
+        security: resolve(__dirname, 'security.html'),
+        employment: resolve(__dirname, 'employment.html'),
+        entertainment: resolve(__dirname, 'entertainment.html'),
+        employment: resolve(__dirname, 'employment.html'),
+        customerservice: resolve(__dirname, 'customerservice.html'),
+        resource: resolve(__dirname, 'resource.html')
       }
     }
   }
