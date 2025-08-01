b12352c81747acb7af3614ed9f120aae57bacce0
Aidan Engler <aidanbope@gmail.com>
2025-08-01 19:23:30 -0400

    Descriptive message about your updates

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,15 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
         automation: resolve(__dirname, 'automation.html'),
-        transportation: resolve(__dirname, 'transportaion.html'),
+        transportation: resolve(__dirname, 'transportation.html'),
         security: resolve(__dirname, 'security.html'),
         employment: resolve(__dirname, 'employment.html'),
         entertainment: resolve(__dirname, 'entertainment.html'),
-        employment: resolve(__dirname, 'employment.html'),
+        education: resolve(__dirname, 'education.html'),
         customerservice: resolve(__dirname, 'customerservice.html'),
+        finance: resolve(__dirname, 'finance.html'),
+        government: resolve(__dirname, 'government.html'),
+        healthcare: resolve(__dirname, 'healthcare.html'),
         resource: resolve(__dirname, 'resource.html')
       }
     }
